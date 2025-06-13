14e497fff722d512a2166d7ba9c2a33d8ff6a129
Dan Martinez <dttez39@gmail.com>
2025-06-13 16:29:45 -0700

    Prueba-Error

--- NewAttempt.py
+++ NewAttempt.py
@@ -1,3 +1,4 @@
 print("Este es mi primer intento de hacer los pasos sin revisar las notas")
 print("Ahora, vamos a probar las caracteristicas de Diff")
 print("Este va a ser el cuarto comando/impresión desde VSCode")
+print("Cambio en Main desde Login para confirmar el error")
