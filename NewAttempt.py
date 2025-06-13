ed0695c0e3a6b79784871ef7808a5fc60580c971
Dan Martinez <dttez39@gmail.com>
2025-06-13 15:15:31 -0700

    Tercer Commit

--- NewAttempt.py
+++ NewAttempt.py
@@ -1,2 +1,3 @@
 print("Este es mi primer intento de hacer los pasos sin revisar las notas")
 print("Ahora, vamos a probar las caracteristicas de Diff")
+print("Este va a ser el cuarto comando/impresión desde VSCode")
