cd2042d6cec89df3f1997be75ce5957e28762653
Dan Martinez <dttez39@gmail.com>
2025-06-13 15:25:47 -0700

    Cuarto Commit

--- NewAttempt.py
+++ NewAttempt.py
@@ -1,3 +1,4 @@
 print("Este es mi primer intento de hacer los pasos sin revisar las notas")
 print("Ahora, vamos a probar las caracteristicas de Diff")
 print("Este va a ser el cuarto comando/impresión desde VSCode")
+print("Apartir de esta posición el proyecto es manejado por 'Login'")
