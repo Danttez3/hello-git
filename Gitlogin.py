505010723c34a9706022a3fb701c069fa1268d3f
Dan Martinez <dttez39@gmail.com>
2025-06-13 17:25:57 -0700

    Conflicto Solucionado

--- Gitlogin.py
+++ Gitlogin.py
@@ -1,1 +1,3 @@
 print("Este es el primer paso del nuevo Login, creando un nuevo archivo")
+print("Este es un ejemplo de 'Stash'")
+print("Utilizando el usuario main, estoy haciendo cambios en Login")
--- NewAttempt.py
+++ NewAttempt.py
@@ -1,3 +1,8 @@
 print("Este es mi primer intento de hacer los pasos sin revisar las notas")
 print("Ahora, vamos a probar las caracteristicas de Diff")
 print("Este va a ser el cuarto comando/impresión desde VSCode")
+<<<<<<< Updated upstream
+=======
+print("Apartir de esta posición el proyecto es manejado por 'Login'")
+print("Desde Usuario Main, estoy haciendo cambios en 'NewAttempt'")
+>>>>>>> Stashed changes
